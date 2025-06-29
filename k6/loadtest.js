89b50bc452a019d8c397549a942dd8f9fee0456c
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2025-06-29 12:45:32 +0700

    lab-1: ramp up, stable, ramp down

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -1,31 +1,29 @@
 import http from "k6/http";
-import { sleep, check } from "k6";
+import { check } from "k6";
 
-export let options = {
-  vus: 100,
-  duration: "30s"
+export const options = {
+  tags: {
+    name: "staged_test_x",
+    type: "loadtest",
+    testid: "workshop-01"
+  },
+  scenarios: {
+    workshop_01: {
+      executor: "ramping-vus",
+      startVUs: 0,
+      stages: [
+        { duration: "72s", target: 10 }, // ramp-up
+        { duration: "72s", target: 10 }, // steady
+        { duration: "36s", target: 0 } // ramp-down
+      ]
+    }
+  }
 };
 
 export default function () {
-  // Get all users
-  let res = http.get("http://api-bad:8080/users");
+  const res = http.get("http://192.168.3.140/api/v1/product/1");
   check(res, {
     "status is 200": (r) => r.status === 200,
     "body is not empty": (r) => r.body.length > 0
   });
-
-  // Get user by ID with specific ID
-  res = http.get("http://api-bad:8080/users/1");
-  check(res, {
-    "status is 200": (r) => r.status === 200,
-    "body is not empty": (r) => r.body.length > 0
-  });
-
-  // Get user by ID with random ID
-  // let userId = Math.floor(Math.random() * 90000) + 1; // Assuming IDs are 1-90000
-  // res = http.get(`http://api-bad:8080/users/${userId}`);
-  // check(res, {
-  //     'status is 200': (r) => r.status === 200,
-  //     'body is not empty': (r) => r.body.length > 0,
-  // });
 }
---
+++ k6/loadtest_template.js
@@ -0,0 +1,31 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export let options = {
+  vus: 100,
+  duration: "30s"
+};
+
+export default function () {
+  // Get all users
+  let res = http.get("http://api-bad:8080/users");
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+    "body is not empty": (r) => r.body.length > 0
+  });
+
+  // Get user by ID with specific ID
+  res = http.get("http://api-bad:8080/users/1");
+  check(res, {
+    "status is 200": (r) => r.status === 200,
+    "body is not empty": (r) => r.body.length > 0
+  });
+
+  // Get user by ID with random ID
+  // let userId = Math.floor(Math.random() * 90000) + 1; // Assuming IDs are 1-90000
+  // res = http.get(`http://api-bad:8080/users/${userId}`);
+  // check(res, {
+  //     'status is 200': (r) => r.status === 200,
+  //     'body is not empty': (r) => r.body.length > 0,
+  // });
+}
